0f9134b9e0b0795824d9dfe826376c7550fed37e
deallves <deallves@gmail.com>
2022-09-01 23:04:48 -0300

    finally

--- src/FluxoComTratamento.java
+++ src/FluxoComTratamento.java
@@ -1,6 +1,6 @@
 public class FluxoComTratamento {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws MinhaExcecao {
 		System.out.println("Ini do main");
 		try {
 			metodo1();
@@ -14,7 +14,7 @@ public class FluxoComTratamento {
 		System.out.println("Fim do main");
 	}
 
-	private static void metodo1() {
+	private static void metodo1() throws MinhaExcecao {
 		System.out.println("Ini do metodo1");
 
 		metodo2(); 
@@ -22,7 +22,7 @@ public class FluxoComTratamento {
 		System.out.println("Fim do metodo1");
 	}
 
-	private static void metodo2() {
+	private static void metodo2() throws MinhaExcecao {
 		System.out.println("Ini do metodo2");
 		for (int i = 1; i <= 5; i++) {
 			System.out.println(i);
